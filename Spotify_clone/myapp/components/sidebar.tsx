3befcc58d057f50dda0b1c0a186ee0501a58c3cb
siddhant tripathy <siddhant.tripathy7@gmail.com>
2022-06-14 19:00:11 +0530

    App Layout

---
+++ Spotify_clone/myapp/components/sidebar.tsx
@@ -0,0 +1,38 @@
+import {
+    List,
+    Box,
+    ListItem,
+    ListIcon,
+    Divider,
+    Center,
+    LinkBox,
+    LinkOverlay
+  } from '@chakra-ui/layout'
+  import {
+    MdHome,
+    MdSearch,
+    MdLibraryMusic,
+    MdPlaylistAdd,
+    MdFavorite,
+  } from 'react-icons/md'
+const Sidebar = () => {
+return (
+    
+  <Box width= "100%"
+  height="clac(100vh-100px)"
+  bg="black"
+  paddingX="5px"
+  color="gray" >
+    <Box paddingY="20px">
+        <Box width ="120px" marginBottom="20px" paddingX="20px">
+            
+        </Box>
+    </Box>
+   </Box>
+
+)    
+
+
+}
+
+export default Sidebar 
