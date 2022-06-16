ba0a0654de55bb0a147746d5d045747e5f6a7018
siddhant tripathy <siddhant.tripathy7@gmail.com>
2022-06-16 17:46:37 +0530

    just adding box model

--- Spotify_clone/myapp/components/playerLayout.tsx
+++ Spotify_clone/myapp/components/playerLayout.tsx
@@ -3,8 +3,8 @@ import Sidebar from './sidebar'
 
 const PlayerLayout = ({ children }) => {
     return (
-<Box width="100vw" height="100vh">
-    <Box position="absolute" top="0" width="250pxs" left="0">
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" width="250px" left="0">
     <Sidebar/>
 </Box>
       <Box marginLeft="250px" marginBottom="100px">
--- Spotify_clone/myapp/components/sidebar.tsx
+++ Spotify_clone/myapp/components/sidebar.tsx
@@ -1,3 +1,4 @@
+import NextImage from 'next/image'
 import {
     List,
     Box,
@@ -18,21 +19,20 @@ import {
 const Sidebar = () => {
 return (
     
-  <Box width= "100%"
-  height="clac(100vh-100px)"
-  bg="black"
-  paddingX="5px"
-  color="gray" >
-    <Box paddingY="20px">
+  <Box 
+      width= "100%"
+      height="  calc(100vh - 100px)"
+      bg="black"
+      paddingX="5px"
+      color="gray" >
+      <Box paddingY="20px" height="100%">
         <Box width ="120px" marginBottom="20px" paddingX="20px">
-            
+           <NextImage src = "/Music-Player.svg" height ={60} width ={120} />
         </Box>
     </Box>
    </Box>
 
 )    
-
-
 }
 
 export default Sidebar 
